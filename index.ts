20ea1c83ffbbb5c0d39e665cd059a569cd1af1f6
gtopsis <csd3004@csd.uoc.gr>
2023-11-18 22:21:13 +0200

    remove node_modules from remote

--- index.ts
+++ index.ts
@@ -1,16 +1,18 @@
-import express, { Express, Request, Response , Application } from 'express';
-import dotenv from 'dotenv';
+import express, { Express, Request, Response, Application } from "express";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import cors from "cors";
 
-//For env File 
+//For env File
 dotenv.config();
 
 const app: Application = express();
 const port = process.env.PORT || 8000;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Welcome to Express & TypeScript Server');
-});
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
 
 app.listen(port, () => {
-  console.log(`Server is Fire at http://localhost:${port}`);
+  console.log(`Listening on port ${PORT}`);
 });
