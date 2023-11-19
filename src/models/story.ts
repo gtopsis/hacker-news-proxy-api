34f1f1a60119526514ee7c5fa4426783f4d11027
gtopsis <csd3004@csd.uoc.gr>
2023-11-19 06:48:31 +0200

    feat: add data models

---
+++ src/models/story.ts
@@ -0,0 +1,13 @@
+export type Stories = string[];
+
+export interface Story {
+  by: string;
+  descendants: number;
+  id: number;
+  kids: number[];
+  score: number;
+  time: number;
+  title: string;
+  type: string;
+  url: string;
+}
