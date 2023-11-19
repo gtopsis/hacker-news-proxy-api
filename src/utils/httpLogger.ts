0c1228cc13216f6c8c52aab2de130719d2b25ff7
gtopsis <csd3004@csd.uoc.gr>
2023-11-19 10:07:11 +0200

    chore: config http logger

--- src/utils/httpLogger.ts
+++ src/utils/httpLogger.ts
@@ -1,13 +1,5 @@
 const morgan = require("morgan");
-const fs = require("fs");
-const path = require("node:path");
 
-// Create a write stream (in append mode)
-const accessLogStream = fs.createWriteStream(
-  path.join(__dirname, "access.log"),
-  { flags: "a" }
-);
-
-const httpLogger = morgan("combined", { stream: accessLogStream });
+const httpLogger = morgan("tiny");
 
 export default httpLogger;
