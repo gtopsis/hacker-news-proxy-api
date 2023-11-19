3b172c5ab16a4b5335d61070dd02fdda239e7f2e
gtopsis <csd3004@csd.uoc.gr>
2023-11-19 12:44:14 +0200

    feat: add client service for http requests

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import app from "./app";
 import logger from "./utils/logger";
-
-const port = process.env.PORT || 8000;
+import { port } from "./utils/config";
 
 app.listen(port, () => {
   logger.info(`Listening on port ${port}`);
