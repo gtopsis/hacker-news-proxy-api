1e85c993c55d78f6ee45f143464650800703c085
gtopsis <csd3004@csd.uoc.gr>
2023-11-20 12:53:14 +0200

    refactor

--- src/config/app.ts
+++ src/config/app.ts
@@ -2,8 +2,8 @@ import express, { Express, Request, Response, Application } from "express";
 import dotenv from "dotenv";
 const helmet = require("helmet");
 import cors from "cors";
-import newsRouter from "./router";
-import httpLogger from "./utils/httpLogger";
+import newsRouter from "../router";
+import httpLogger from "../utils/httpLogger";
 
 dotenv.config();
 
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(express.json());
 app.use(httpLogger);
 
-// routes
+// routesvscode
 app.use("/news", newsRouter);
 
 export default app;
