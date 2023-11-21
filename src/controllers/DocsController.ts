3f55790df8361eaf51a8e78d59182685e30bc9f1
gtopsis <csd3004@csd.uoc.gr>
2023-11-21 17:43:24 +0200

    fix: path for API doc on relative endpoint

--- src/controllers/DocsController.ts
+++ src/controllers/DocsController.ts
@@ -1,13 +1,16 @@
 import { NextFunction } from "express";
 import { Request, Response } from "express";
 import fs from "fs";
+import { resolve } from "path";
 
 export const getAPIDocs = async (
   request: Request,
   response: Response,
   next: NextFunction
 ) => {
-  const yamlData = fs.readFileSync("./docs/api-definition-base.yml");
+  const yamlData = fs.readFileSync(
+    resolve(__dirname, "../../docs/api-definition-base.yaml")
+  );
 
   response.header("Content-Type", "text/yaml");
   response.send(yamlData);
