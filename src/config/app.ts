afaf2188a6e54b20c24efe777fb78dc980ed00ab
gtopsis <csd3004@csd.uoc.gr>
2023-11-23 19:43:42 +0200

    feat: catch missing routes

--- src/config/app.ts
+++ src/config/app.ts
@@ -1,10 +1,12 @@
-import express, { Express, Request, Response, Application } from "express";
+import express, { Request, Response, Application, NextFunction } from "express";
 const helmet = require("helmet");
 import cors from "cors";
 import newsRouter from "../routes/NewsRouter";
 import apiDocRouter from "../routes/apiDocRouter";
 import httpLogger from "../utils/httpLogger";
 import errorMiddleware from "../middlewares/error";
+import { APIError } from "../utils/APIError";
+import { HttpStatusCode } from "axios";
 
 const app: Application = express();
 
@@ -15,8 +17,12 @@ app.use(express.json());
 app.use(httpLogger);
 
 // routes
-app.use("/news", newsRouter);
 app.use("/", apiDocRouter);
+app.use("/news", newsRouter);
+
+app.all("*", (req: Request, res: Response, next: NextFunction) => {
+  next(new APIError("Route not found", "", HttpStatusCode.NotFound));
+});
 
 app.use(errorMiddleware);
 
